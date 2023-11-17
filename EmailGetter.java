999a732d9fdd9c1a340587b4839df018bdd23ead
Kamil <tagirov2004@bk.ru>
2023-11-17 16:39:14 +0300

    Update EmailGetter

--- EmailGetter.java
+++ EmailGetter.java
@@ -3,6 +3,14 @@ import java.util.Properties;
 import javax.mail.*;
 import javax.activation.*;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+
 public class EmailSender {
     public static void main(String[] args) {
         final String username = "tagirov@bk.ru";
@@ -50,6 +58,45 @@ public class EmailSender {
             store.close();
         } catch (Exception e) {
             e.printStackTrace();
+        }
+		
+		String filePath = "C:\\Users\\User\\Desktop\\exported_data.xml";
+
+        try {
+            printXmlFileContent(filePath);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+	
+	    public static void printXmlFileContent(String filePath) throws Exception {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+
+        // Считываем документ из файла
+        Document document = builder.parse(new File(filePath));
+
+        // Выводим содержимое документа
+        printNode(document, "");
+    }
+
+    private static void printNode(Node node, String indent) {
+        // Выводим информацию о текущем узле
+        System.out.print(indent + node.getNodeName());
+
+        if (node.getNodeValue() != null && !node.getNodeValue().trim().isEmpty()) {
+            System.out.print(": " + node.getNodeValue().trim());
+        }
+
+        System.out.println();
+
+        // Рекурсивно обходим дочерние узлы
+        NodeList nodeList = node.getChildNodes();
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node childNode = nodeList.item(i);
+            if (childNode.getNodeType() == Node.ELEMENT_NODE) {
+                printNode(childNode, indent + "  ");
+            }
         }
     }
 }
